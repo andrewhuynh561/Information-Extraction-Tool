aa6f3cf80fc7bc0f353cdfe21a589e694cc9a643
Chanh Hai Huynh <huyhc001@mymail.unisa.edu.au>
2024-05-29 20:09:15 +0930

    getting bordoer box of images with 2 methods

--- test1.py
+++ test1.py
@@ -13,9 +13,15 @@ model.overrides['max_det'] = 1000  # maximum number of detections per image
 image = 'images/page_1.png'
 
 # perform inference
-results = model.predict(image)
+results = model.predict(image,save_crop=True)
 
-# observe results
-print(results[0].boxes)
+# #print coordinates
+# print("Detected boxes coordinates:")
+# for box in results[0].boxes:
+#     x1, y1, x2, y2 = box.xyxy[0]  # Extract coordinates
+#     print(f"Box: ({x1}, {y1}), ({x2}, {y2})")
+
+# # observe results
+# print(results[0].boxes)
 render = render_result(model=model, image=image, result=results[0])
 render.show()
